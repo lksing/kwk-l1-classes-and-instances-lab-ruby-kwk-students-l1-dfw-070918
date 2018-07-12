6e3c90c0ec129e4e604a813f9c8a0e79aef6def5
lksing <leiaxsing@gmail.com>
2018-07-12 21:02:33 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 # dog.rb
 
 class Dog
-  def initialize(name)
-    @name = name
-  end
+  
+  
+
 end
 
-dog1 = Dog.new("fido")
-dog2 = Dog.new("snoopy")
-dog3 = Dog.new("lassie")
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
